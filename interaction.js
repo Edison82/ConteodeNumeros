ab348833e7ea897a2027d1278a82daa71c71329e
Edison82 <ediflow82@gmail.com>
2022-01-16 11:09:02 +0100

    Conteo de numeros

---
+++ interaction.js
@@ -0,0 +1,18 @@
+function Tres(){
+
+    for(var a = 0; a <=20; a +=3){
+        console.log("De 3 en 3 = "+ a);
+    }
+}
+
+function Dos(){
+    let c = 0;
+    while(c<20){
+        c+=2;
+        console.log("numeros 2 en 2 = " + c);
+    }
+}
+console.log( Tres() +  Dos() );
+
+let c = 0;
+
